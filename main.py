b42d7880f96ce14fb27354e7f81e07016f3fa836
Anime-Shhh <animeshjadhav05@gmail.com>
2025-01-16 16:28:50 -0500

    added methods to find the character counts and report functionality

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-def main():
-    bookPath = "books/frankenstein.txt"
-    text = getBookText(bookPath)
-    # print(text)
-    print(countWords(text))
-
-
 def getBookText(bookPath):
     with open(bookPath) as file:
         text = file.read()
@@ -13,6 +6,37 @@ def getBookText(bookPath):
 
 def countWords(text):
     return len(text.split())
+
+
+def countCharacters(text):
+    seen = set()
+    countLib = {}
+    loweredText = text.lower()
+    for character in loweredText:
+        if character.isalpha():
+            if character not in seen:
+                seen.add(character)
+                countLib[character] = 1
+            else:
+                countLib[character] += 1
+
+    #now sort the dictionary
+    countLib = dict(sorted(countLib.items(), key=lambda item: item[1], reverse=True))
+    return countLib
+
+
+def printCharacters(characterCounts):
+    for character in characterCounts:
+        print(f"The '{character}' character was found {characterCounts[character]} times")
+
+
+def main():
+    bookPath = "books/frankenstein.txt"
+    text = getBookText(bookPath)
+    print(f"--- Begin the report of {bookPath} ---")
+    print(f"{countWords(text)} words were found in the document\n")
+    printCharacters(countCharacters(text))    
+    print("--- End report ---")
 
 
 main()
